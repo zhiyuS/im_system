4d29bf5b61f19c695f55ebd66a10d30ce7ad114a
cj <caojie6058@163.com>
2023-03-11 15:52:02 +0800

    sign加密拦截

---
+++ service/src/main/java/com/cj/im/service/config/WebConfig.java
@@ -0,0 +1,39 @@
+package com.lld.im.service.config;
+
+import com.lld.im.service.interceptor.GateWayInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @description:
+ * @author: lld
+ * @version: 1.0
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    GateWayInterceptor gateWayInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(gateWayInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/v1/user/login")
+                .excludePathPatterns("/v1/message/checkSend");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
+
+}
